dc3e7955a6d851d5561bd08cd1e199f31f086655
Pablobc <pablobecerra120@gmail.com>
2022-10-07 03:08:48 -0300

    avance

--- DES.py
+++ DES.py
@@ -3,6 +3,11 @@
 #plaintext to hexadecimal
 import random
 
+bloquept = 64
+mediobloquept = int(bloquept/2)
+tamaniollave = 64
+tamaniosubllave = 48
+
 def generatePermTab(n):
 	table=[*range(1,n+1,1)]
 	for i in range(len(table)-1):
@@ -11,13 +16,22 @@ def generatePermTab(n):
 		table.append(temp)
 	return table
 	
+def generateExpTab(mediobloquept, tamaniosubllave):
+	values= [*range(1,mediobloquept+1, 1)]
+	exp=[]
+	for i in range(tamaniosubllave):
+		exp.append(values[random.randint(0,(len(values)-1))])
+	return exp
 
 def pt2hex(s):
 	hex_s = s.encode('utf-8')
 	hex_s = hex_s.hex()
 	return hex_s.upper()
 
-
+def inverse_permutation(a):
+    b = np.arange(a.shape[0])
+    b[a] = b.copy()
+    return b
 
 # Hexadecimal to binary conversion
 
@@ -140,43 +154,18 @@ def sbox(sixbits):
 	return sixbits[len(sixbits)- 2:]
 
 # Table of Position of 64 bits at initial level: Initial Permutation Table
-initial_perm = [58, 50, 42, 34, 26, 18, 10, 2,
-				60, 52, 44, 36, 28, 20, 12, 4,
-				62, 54, 46, 38, 30, 22, 14, 6,
-				64, 56, 48, 40, 32, 24, 16, 8,
-				57, 49, 41, 33, 25, 17, 9, 1,
-				59, 51, 43, 35, 27, 19, 11, 3,
-				61, 53, 45, 37, 29, 21, 13, 5,
-				63, 55, 47, 39, 31, 23, 15, 7]
+initial_perm = generatePermTab(bloquept)
 
 # Expansion D-box Table
-exp_d = [32, 1, 2, 3, 4, 5, 4, 5,
-		6, 7, 8, 9, 8, 9, 10, 11,
-		12, 13, 12, 13, 14, 15, 16, 17,
-		16, 17, 18, 19, 20, 21, 20, 21,
-		22, 23, 24, 25, 24, 25, 26, 27,
-		28, 29, 28, 29, 30, 31, 32, 1]
+exp_d = generateExpTab(mediobloquept,tamaniosubllave)
 
 # Straight Permutation Table
-per = [16, 7, 20, 21,
-	29, 12, 28, 17,
-	1, 15, 23, 26,
-	5, 18, 31, 10,
-	2, 8, 24, 14,
-	32, 27, 3, 9,
-	19, 13, 30, 6,
-	22, 11, 4, 25]
+per = generatePermTab(mediobloquept)
 
 
 # Final Permutation Table
-final_perm = [40, 8, 48, 16, 56, 24, 64, 32,
-			39, 7, 47, 15, 55, 23, 63, 31,
-			38, 6, 46, 14, 54, 22, 62, 30,
-			37, 5, 45, 13, 53, 21, 61, 29,
-			36, 4, 44, 12, 52, 20, 60, 28,
-			35, 3, 43, 11, 51, 19, 59, 27,
-			34, 2, 42, 10, 50, 18, 58, 26,
-			33, 1, 41, 9, 49, 17, 57, 25]
+final_perm = initial_perm
+final_perm.reverse()
 
 
 def encrypt(pt, rkb, rk):
@@ -184,6 +173,7 @@ def encrypt(pt, rkb, rk):
 
 	# Initial Permutation
 	pt = permute(pt, initial_perm, 64)
+	print(pt)
 	print("After initial permutation", bin2hex(pt))
 
 	# Splitting
@@ -225,24 +215,13 @@ def encrypt(pt, rkb, rk):
 
 entrada = "123RADWAASDADKSA"
 key = "AABB09182736CCDD"
+
+
 pt = pt2hex(entrada)
 print(pt)
 # Key generation
 # --hex to binary
 key = hex2bin(key)
-
-# --parity bit drop table
-keyp = [57, 49, 41, 33, 25, 17, 9,
-		1, 58, 50, 42, 34, 26, 18,
-		10, 2, 59, 51, 43, 35, 27,
-		19, 11, 3, 60, 52, 44, 36,
-		63, 55, 47, 39, 31, 23, 15,
-		7, 62, 54, 46, 38, 30, 22,
-		14, 6, 61, 53, 45, 37, 29,
-		21, 13, 5, 28, 20, 12, 4]
-
-# getting 56 bit key from 64 bit using the parity bits
-key = permute(key, keyp, 56)
 
 # Number of bit shifts
 shift_table = [1, 1, 2, 2,
@@ -251,21 +230,14 @@ shift_table = [1, 1, 2, 2,
 			2, 2, 2, 1]
 
 # Key- Compression Table : Compression of key from 56 bits to 48 bits
-key_comp = [14, 17, 11, 24, 1, 5,
-			3, 28, 15, 6, 21, 10,
-			23, 19, 12, 4, 26, 8,
-			16, 7, 27, 20, 13, 2,
-			41, 52, 31, 37, 47, 55,
-			30, 40, 51, 45, 33, 48,
-			44, 49, 39, 56, 34, 53,
-			46, 42, 50, 36, 29, 32]
+key_comp = generatePermTab(tamaniosubllave)
 
 # Splitting
-left = key[0:28] # rkb for RoundKeys in binary
-right = key[28:56] # rk for RoundKeys in hexadecimal
+left = key[0:int(tamaniollave/2)] 
+right = key[int(tamaniollave/2):tamaniollave] 
 
-rkb = []
-rk = []
+rkb = [] # rkb for RoundKeys in binary
+rk = [] # rk for RoundKeys in hexadecimal
 for i in range(0, 16):
 	# Shifting the bits by nth shifts by checking from shift table
 	left = shift_left(left, shift_table[i])
@@ -275,7 +247,7 @@ for i in range(0, 16):
 	combine_str = left + right
 
 	# Compression of key from 56 to 48 bits
-	round_key = permute(combine_str, key_comp, 48)
+	round_key = permute(combine_str, key_comp, tamaniosubllave)
 
 	rkb.append(round_key)
 	rk.append(bin2hex(round_key))
